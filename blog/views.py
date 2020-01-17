4b6a60bedd15c011c7e237e4f728e9a10ec22ede
Luis Quiros <luis.quiros@avantica.net>
2020-01-17 10:09:12 -0600

    Part 3
    Templates, css and data injection to HTML

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,24 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+posts = [
+    {
+        'author': 'Luis',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'January 17, 2020'
+    },
+    {
+        'author': 'Adriana',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'January 17, 2020'
+    }
+]
 
 
 def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+    return render(request, 'blog/home.html', {'posts': posts})
+
 
 def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+    return render(request, 'blog/about.html', {'title': 'About'})
