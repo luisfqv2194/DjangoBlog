ab710891ab1be1d9b39ff39d0e345263bb2fa387
Luis Quiros <luis.quiros@avantica.net>
2020-01-17 13:33:16 -0600

    Part 4
    Dummy data removed for DB data injection, new model added to admin site, plus some migrations updates

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-posts = [
-    {
-        'author': 'Luis',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'January 17, 2020'
-    },
-    {
-        'author': 'Adriana',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'January 17, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
-    return render(request, 'blog/home.html', {'posts': posts})
+    return render(request, 'blog/home.html', {'posts': Post.objects.all()})
 
 
 def about(request):
